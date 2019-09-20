21b8838fe638372081b2779b75404ffe62ea013d
Víctor Manuel Sánchez Cartagena <vmsanchez@tobago>
2019-09-20 15:02:29 +0200

    properly detach

--- aevnmt/models/aevnmt.py
+++ aevnmt/models/aevnmt.py
@@ -393,7 +393,8 @@ class AEVNMT(nn.Module):
 
         KL_prediction=0
         if qz_prediction is not None:
-            KL_prediction=torch.distributions.kl.kl_divergence(qz.detach(),qz_prediction)
+            qz_copy=torch.distributions.Normal(qz.loc.detach(),qz.scale.detach())
+            KL_prediction=torch.distributions.kl.kl_divergence(qz_copy,qz_prediction)
             KL_prediction=KL_prediction.sum(dim=1)
 
         if free_nats > 0:
