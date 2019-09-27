75b483a741851a07cf0a8702ad7432a4c4e82240
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-09-27 14:51:24 +0200

    Added missing new files

---
+++ aevnmt/models/vae.py
@@ -0,0 +1,246 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from aevnmt.components import RNNEncoder, tile_rnn_hidden, tile_rnn_hidden_for_decoder
+from aevnmt.dist import NormalLayer
+
+from itertools import chain
+
+class InferenceNetwork(nn.Module):
+
+    def __init__(self, src_embedder, hidden_size, latent_size, bidirectional, num_enc_layers, cell_type):
+        """
+        :param src_embedder: uses this embedder, but detaches its output from the graph as to not compute
+                             gradients for it.
+        """
+        super().__init__()
+        self.src_embedder = src_embedder
+        emb_size = src_embedder.embedding_dim
+        self.encoder = RNNEncoder(emb_size=emb_size,
+                                  hidden_size=hidden_size,
+                                  bidirectional=bidirectional,
+                                  dropout=0.,
+                                  num_layers=num_enc_layers,
+                                  cell_type=cell_type)
+        encoding_size = hidden_size if not bidirectional else hidden_size * 2
+        self.normal_layer = NormalLayer(encoding_size, hidden_size, latent_size)
+
+    def forward(self, x, seq_mask_x, seq_len_x):
+        x_embed = self.src_embedder(x).detach()
+        encoder_outputs, _ = self.encoder(x_embed, seq_len_x)
+        avg_encoder_output = (encoder_outputs * seq_mask_x.unsqueeze(-1).type_as(encoder_outputs)).sum(dim=1)
+        return self.normal_layer(avg_encoder_output)
+
+    def parameters(self, recurse=True):
+        return chain(self.encoder.parameters(recurse=recurse), self.normal_layer.parameters(recurse=recurse))
+
+    def named_parameters(self, prefix='', recurse=True):
+        return chain(self.encoder.named_parameters(prefix='', recurse=True), self.normal_layer.named_parameters(prefix='', recurse=True), )
+
+class VAE(nn.Module):
+
+    def __init__(self, emb_size, latent_size, hidden_size, bidirectional,num_layers,cell_type, language_model, pad_idx, dropout):
+        super().__init__()
+        self.latent_size = latent_size
+        self.pad_idx = pad_idx
+
+        self.language_model = language_model
+
+        self.dropout_layer = nn.Dropout(p=dropout)
+
+        self.lm_init_layer = nn.Sequential(nn.Linear(latent_size, language_model.hidden_size),
+                                           nn.Tanh())
+
+        self.inf_network = InferenceNetwork(src_embedder=self.language_model.embedder,
+                                                hidden_size=hidden_size,
+                                                latent_size=latent_size,
+                                                bidirectional=bidirectional,
+                                                num_enc_layers=num_layers,
+                                                cell_type=cell_type)
+        self.pred_network=self.inf_network
+        # This is done because the location and scale of the prior distribution are not considered
+        # parameters, but are rather constant. Registering them as buffers still makes sure that
+        # they will be moved to the appropriate device on which the model is run.
+        self.register_buffer("prior_loc", torch.zeros([latent_size]))
+        self.register_buffer("prior_scale", torch.ones([latent_size]))
+
+    def inference_parameters(self):
+        return self.inf_network.parameters()
+
+    def generative_parameters(self):
+        # TODO: separate the generative model into a GenerativeModel module
+        #  within that module, have two modules, namely, LanguageModel and TranslationModel
+        return self.lm_parameters()
+
+    def lm_parameters(self):
+        return chain(self.language_model.parameters(), self.lm_init_layer.parameters())
+
+
+    def approximate_posterior(self, x, seq_mask_x, seq_len_x, y, seq_mask_y, seq_len_y):
+        """
+        Returns an approximate posterior distribution q(z|x).
+        """
+
+        return self.inf_network(x, seq_mask_x, seq_len_x)
+
+    def approximate_posterior_prediction(self, x, seq_mask_x, seq_len_x):
+        """
+        Returns an approximate posterior distribution q(z|x).
+        """
+
+        return self.pred_network(x, seq_mask_x, seq_len_x)
+
+    def prior(self):
+        return torch.distributions.Normal(loc=self.prior_loc,
+                                          scale=self.prior_scale)
+
+    def src_embed(self, x):
+
+        # We share the source embeddings with the language_model.
+        x_embed = self.language_model.embedder(x)
+        x_embed = self.dropout_layer(x_embed)
+        return x_embed
+
+
+    def encode(self, x, seq_len_x, z):
+        x_embed = self.src_embed(x)
+        hidden = tile_rnn_hidden(self.encoder_init_layer(z), self.encoder.rnn)
+        return self.encoder(x_embed, seq_len_x, hidden=hidden)
+
+
+    def lm_generate(self, pre_output):
+        return pre_output
+
+    def init_lm(self, z):
+        hidden = tile_rnn_hidden(self.lm_init_layer(z), self.language_model.rnn)
+        return hidden
+
+    def run_language_model(self, x, z):
+        """
+        Runs the language_model.
+
+        :param x: unembedded source sentence
+        :param z: a sample of the latent variable
+        """
+        hidden = tile_rnn_hidden(self.lm_init_layer(z), self.language_model.rnn)
+        return self.language_model(x, hidden=hidden)
+
+    def forward(self, x, seq_mask_x, seq_len_x, y, z):
+
+        # Estimate the Categorical parameters for E[P(x|z)] using the given sample of the latent
+        # variable.
+        lm_logits = self.run_language_model(x, z)
+
+        return None, lm_logits, None
+
+    def compute_conditionals(self, x_in, seq_mask_x, seq_len_x, x_out, y_in, y_out, z):
+        """
+        :param x_in: [batch_size, max_length]
+        :param seq_mask_x: [batch_size, max_length]
+        :param seq_len_x: [batch_size]
+        :param x_out: [batch_size, max_length]
+        :param y_in: [batch_size, max_length]
+        :param y_out: [batch_size, max_length]
+        :param z: [batch_size, latent_size]
+        :return: log p(x|z), log p(y|z,x)
+        """
+        # Encode the source sentence and initialize the decoder hidden state.
+        encoder_outputs, encoder_final = self.encode(x_in, seq_len_x, z)
+        hidden = self.init_decoder(encoder_outputs, encoder_final, z)
+
+        # Estimate the Categorical parameters for E[P(x|z)] using the given sample of the latent
+        # variable.
+        # [max_length, batch_size, vocab_size]
+        lm_logits = self.run_language_model(x_in, z)
+
+        # [batch_size, max_length, vocab_size]
+        lm_logits = lm_logits.permute(0, 2, 1)
+
+        # [batch_size]
+        lm_loss = F.cross_entropy(lm_logits, x_out, ignore_index=self.pad_idx, reduction="none").sum(dim=1)
+
+        return -lm_loss, 0
+
+    def compute_lm_likelihood(self, x_in, seq_mask_x, seq_len_x, x_out, z):
+        """
+        :param x_in: [batch_size, max_length]
+        :param seq_mask_x: [batch_size, max_length]
+        :param seq_len_x: [batch_size]
+        :param x_out: [batch_size, max_length]
+        :param y_in: [batch_size, max_length]
+        :param y_out: [batch_size, max_length]
+        :param z: [batch_size, latent_size]
+        :return: log p(x|z), log p(y|z,x)
+        """
+        # Encode the source sentence and initialize the decoder hidden state.
+        encoder_outputs, encoder_final = self.encode(x_in, seq_len_x, z)
+        hidden = self.init_decoder(encoder_outputs, encoder_final, z)
+
+        # Estimate the Categorical parameters for E[P(x|z)] using the given sample of the latent
+        # variable.
+        # [max_length, batch_size, vocab_size]
+        lm_logits = self.run_language_model(x_in, z)
+        # [batch_size, max_length, vocab_size]
+        lm_logits = lm_logits.permute(0, 2, 1)
+
+        lm_loss = F.cross_entropy(lm_logits, x_out, ignore_index=self.pad_idx, reduction="none").sum(dim=1)
+
+        return -lm_loss
+
+
+    #TODO: remove tm_logits? target_y?
+    def loss(self, tm_logits, lm_logits, targets_y, targets_x, qz, free_nats=0.,
+             KL_weight=1., reduction="mean", qz_prediction=None):
+        """
+        Computes an estimate of the negative evidence lower bound for the single sample of the latent
+        variable that was used to compute the categorical parameters, and the distributions qz
+        that the sample comes from.
+
+        :param tm_logits: translation model logits, the unnormalized translation probabilities [B, T_y, vocab_size]
+        :param lm_logits: language model logits, the unnormalized language probabilities [B, T_x, vocab_size]
+        :param targets_y: target labels target sentence [B, T_y]
+        :param targets_x: target labels source sentence [B, T_x]
+        :param qz: distribution that was used to sample the latent variable.
+        :param free_nats: KL = min(free_nats, KL)
+        :param KL_weight: weight to multiply the KL with, applied after free_nats
+        :param reduction: what reduction to apply, none ([B]), mean ([]) or sum ([])
+        """
+
+        # Compute the language model categorical loss.
+        lm_loss = self.language_model.loss(lm_logits, targets_x, reduction="none")
+
+        # Compute the KL divergence between the distribution used to sample z, and the prior
+        # distribution.
+        pz = self.prior().expand(qz.mean.size())
+
+        # The loss is the negative ELBO.
+        lm_log_likelihood = -lm_loss
+
+        KL = torch.distributions.kl.kl_divergence(qz, pz)
+        raw_KL = KL.sum(dim=1)
+        KL = KL.sum(dim=1)
+
+        if free_nats > 0:
+            KL = torch.clamp(KL, min=free_nats)
+        KL *= KL_weight
+        elbo = lm_log_likelihood - KL
+        loss = -elbo
+
+        out_dict = {
+            'lm_log_likelihood': lm_log_likelihood,
+            'KL': KL,
+            'raw_KL': raw_KL
+        }
+
+        # Return differently according to the reduction setting.
+        if reduction == "mean":
+            out_dict['loss'] = loss.mean()
+        elif reduction == "sum":
+            out_dict['loss'] = loss.sum()
+        elif reduction == "none":
+            out_dict['loss'] = loss
+        else:
+            raise Exception(f"Unknown reduction option {reduction}")
+
+        return out_dict
---
+++ aevnmt/vae_helper.py
@@ -0,0 +1,322 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch.distributions.normal import Normal
+
+from aevnmt.data import BucketingParallelDataLoader, PAD_TOKEN, SOS_TOKEN, EOS_TOKEN
+from aevnmt.data import create_batch, batch_to_sentences
+from aevnmt.components import RNNEncoder, beam_search, greedy_decode, sampling_decode
+from aevnmt.models import AEVNMT, RNNLM, VAE
+from .train_utils import create_attention, create_decoder, attention_summary, compute_bleu
+
+from torch.utils.data import DataLoader
+
+
+def _draw_translations(model, val_dl, vocab_src, vocab_tgt, device, hparams):
+    with torch.no_grad():
+        inputs = []
+        references = []
+        model_hypotheses = []
+        for sentences_x, sentences_y in val_dl:
+            input_sentences_y=None
+            if hparams.disable_prediction_network and hparams.separate_prediction_network:
+                input_sentences_y=sentences_y
+            hypothesis_nbest,zs = translate(model, sentences_x, vocab_src, vocab_tgt, device, hparams, deterministic=False,input_sentences_y=input_sentences_y)
+            hypothesis=[ t_nbest[0] for t_nbest in hypothesis_nbest]
+
+            # Keep track of inputs, references and model hypotheses.
+            inputs += sentences_x.tolist()
+            references += sentences_y.tolist()
+            model_hypotheses += hypothesis
+    return inputs, references, model_hypotheses
+
+
+def create_model(hparams, vocab_src, vocab_tgt):
+    rnnlm = RNNLM(vocab_size=vocab_src.size(),
+                  emb_size=hparams.emb_size,
+                  hidden_size=hparams.hidden_size,
+                  pad_idx=vocab_src[PAD_TOKEN],
+                  dropout=hparams.dropout,
+                  num_layers=hparams.num_dec_layers,
+                  cell_type=hparams.cell_type,
+                  tied_embeddings=hparams.tied_embeddings)
+
+    model = VAE(   emb_size=hparams.emb_size,
+                   latent_size=hparams.latent_size,
+                   hidden_size=hparams.hidden_size,
+                   bidirectional=hparams.bidirectional,
+                   num_layers=hparams.num_enc_layers,
+                   cell_type=hparams.cell_type,
+                   language_model=rnnlm,
+                   pad_idx=vocab_tgt[PAD_TOKEN],
+                   dropout=hparams.dropout)
+    return model
+
+def train_step(model, x_in, x_out, seq_mask_x, seq_len_x, noisy_x_in, y_in, y_out, seq_mask_y, seq_len_y, noisy_y_in,
+               hparams, step):
+
+    # Use q(z|x,y) for training to sample a z.
+    qz = model.approximate_posterior(x_in, seq_mask_x, seq_len_x,y_in,seq_mask_y, seq_len_y)
+    z = qz.rsample()
+
+    # Compute the translation and language model logits.
+    tm_logits, lm_logits, _ = model(noisy_x_in, seq_mask_x, seq_len_x, noisy_y_in, z)
+
+    # Do linear annealing of the KL over KL_annealing_steps if set.
+    if hparams.KL_annealing_steps > 0:
+        KL_weight = min(1., (1.0 / hparams.KL_annealing_steps) * step)
+    else:
+        KL_weight = 1.
+
+
+    # Compute the loss.
+    loss = model.loss(tm_logits, lm_logits, y_out, x_out, qz,
+                      free_nats=hparams.KL_free_nats,
+                      KL_weight=KL_weight,
+                      reduction="mean", qz_prediction=None)
+    return loss
+
+def validate(model, val_data, vocab_src, vocab_tgt, device, hparams, step, title='xy', summary_writer=None):
+    model.eval()
+
+    # Create the validation dataloader. We can just bucket.
+    val_dl = DataLoader(val_data, batch_size=hparams.batch_size,
+                        shuffle=False, num_workers=4)
+    val_dl = BucketingParallelDataLoader(val_dl)
+
+    val_ppl, val_NLL, val_KL, val_KL_pred = _evaluate_perplexity(model, val_dl, vocab_src, vocab_tgt, device)
+    #We are not validating BLEU at the moment
+    val_bleu_ref, inputs, refs, hyps = _evaluate_bleu(model, val_dl, vocab_src, vocab_tgt,
+                                                  device, hparams)
+    if hparams.paraphrasing_bleu:
+        val_bleu_orig, inputs_orig, refs_orig, hyps_orig = _evaluate_bleu(model, val_dl, vocab_src, vocab_tgt,                                          device, hparams, compare_with_original=True)
+        val_bleu=val_bleu_ref-val_bleu_orig
+    else:
+        val_bleu_orig=None
+        val_bleu=val_bleu_ref
+
+
+    random_idx = np.random.choice(len(inputs))
+    print(f"direction = {title}\n"
+          f"validation perplexity = {val_ppl:,.2f}"
+          f" -- validation NLL = {val_NLL:,.2f}"
+          f" -- validation BLEU reference= {val_bleu_ref:.2f}"
+          f" -- validation BLEU original = {val_bleu_orig:.2f}"
+          f" -- validation BLEU = {val_bleu:.2f}"
+          f" -- validation KL = {val_KL:.2f}\n"
+          f" -- validation KL (between inf and pred nets) = {val_KL_pred:.2f}\n"
+          f"- Source: {inputs[random_idx]}\n"
+          f"- Target: {refs[random_idx]}\n"
+          f"- Prediction: {hyps[random_idx]}")
+
+    if hparams.draw_translations > 0:
+        random_idx = np.random.choice(len(inputs))
+        dl = DataLoader([val_data[random_idx] for _ in range(hparams.draw_translations)], batch_size=hparams.batch_size, shuffle=False, num_workers=4)
+        dl = BucketingParallelDataLoader(dl)
+        i, r, hs = _draw_translations(model, dl, vocab_src, vocab_tgt, device, hparams)
+        print("Posterior samples")
+        print(f"- Input: {i[0]}")
+        print(f"- Reference: {r[0]}")
+        for h in hs:
+            print(f"- Translation: {h}")
+
+    # Write validation summaries.
+    if summary_writer is not None:
+        summary_writer.add_scalar(f"{title}/validation/NLL", val_NLL, step)
+        summary_writer.add_scalar(f"{title}/validation/BLEU", val_bleu, step)
+        summary_writer.add_scalar(f"{title}/validation/perplexity", val_ppl, step)
+        summary_writer.add_scalar(f"{title}/validation/KL", val_KL, step)
+
+
+    return {'bleu': val_bleu, 'likelihood': -val_NLL, 'nll': val_NLL, 'ppl': val_ppl}
+
+
+def re_sample(model, input_sentences, vocab_src,vocab_tgt, device, hparams, deterministic=True,z=None, use_prior=False,input_sentences_y=None):
+    model.eval()
+    with torch.no_grad():
+        x_in, _, seq_mask_x, seq_len_x = create_batch(input_sentences, vocab_src, device)
+        if input_sentences_y:
+            y_in, _, seq_mask_y, seq_len_y = create_batch(input_sentences_y, vocab_tgt, device)
+
+        if z is None:
+            if input_sentences_y:
+                qz = model.approximate_posterior(x_in, seq_mask_x, seq_len_x,y_in, seq_mask_y, seq_len_y)
+            else:
+                qz = model.approximate_posterior_prediction(x_in, seq_mask_x, seq_len_x)
+            if use_prior:
+                #TODO:We are computing qz and it is not need
+                qz=model.prior().expand(qz.mean.size())
+            z = qz.mean if deterministic else qz.sample()
+
+        hidden = model.init_lm(z)
+
+        if hparams.sample_decoding:
+            raw_hypothesis = sampling_decode(model.language_model, model.src_embed,
+                                           model.lm_generate, hidden,
+                                           None, None,
+                                           seq_mask_x, vocab_src[SOS_TOKEN], vocab_src[EOS_TOKEN],
+                                           vocab_src[PAD_TOKEN], hparams.max_decoding_length,hparams.sample_decoding_nucleus_p)
+        elif hparams.beam_width <= 1:
+            raw_hypothesis = greedy_decode(model.language_model, model.src_embed,
+                                           model.lm_generate, hidden,
+                                           None, None,
+                                           seq_mask_x, vocab_src[SOS_TOKEN], vocab_src[EOS_TOKEN],
+                                           vocab_src[PAD_TOKEN], hparams.max_decoding_length)
+        else:
+            raw_hypothesis = beam_search(model.language_model, model.src_embed, model.lm_generate,
+                                         vocab_src.size(), hidden, None,
+                                         None, seq_mask_x,
+                                         vocab_src[SOS_TOKEN], vocab_src[EOS_TOKEN],
+                                         vocab_src[PAD_TOKEN], hparams.beam_width,
+                                         hparams.length_penalty_factor,
+                                         hparams.max_decoding_length,hparams.n_best)
+
+    hypothesis_l=[]
+    for n in range(raw_hypothesis.size(1)):
+        hypothesis_l.append(batch_to_sentences(raw_hypothesis[:,n,:], vocab_src))
+
+    return np.array(hypothesis_l).transpose(1, 0),z
+
+def translate(model, input_sentences, vocab_src, vocab_tgt, device, hparams, deterministic=True,z=None,use_prior=False,input_sentences_y=None):
+    model.eval()
+    with torch.no_grad():
+        x_in, _, seq_mask_x, seq_len_x = create_batch(input_sentences, vocab_src, device)
+        if input_sentences_y is not None:
+            y_in, _, seq_mask_y, seq_len_y = create_batch(input_sentences_y, vocab_tgt, device)
+
+        if z is None:
+            # For translation we use the approximate posterior mean.
+            if input_sentences_y is not None:
+                qz = model.approximate_posterior(x_in, seq_mask_x, seq_len_x,y_in, seq_mask_y, seq_len_y)
+            else:
+                qz = model.approximate_posterior_prediction(x_in, seq_mask_x, seq_len_x)
+            if use_prior:
+                #TODO:We are computing qz and it is not need
+                qz=model.prior().expand(qz.mean.size())
+            z = qz.mean if deterministic else qz.sample()
+        else:
+            #Ensure it is loaded in GPU
+            z=z.to(x_in.device)
+
+        encoder_outputs, encoder_final = model.encode(x_in, seq_len_x, z)
+        hidden = model.init_decoder(encoder_outputs, encoder_final, z)
+
+        if hparams.sample_decoding:
+            raw_hypothesis = sampling_decode(model.decoder, model.tgt_embed,
+                                           model.generate, hidden,
+                                           encoder_outputs, encoder_final,
+                                           seq_mask_x, vocab_tgt[SOS_TOKEN], vocab_tgt[EOS_TOKEN],
+                                           vocab_tgt[PAD_TOKEN], hparams.max_decoding_length,hparams.sample_decoding_nucleus_p)
+        elif hparams.beam_width <= 1:
+            raw_hypothesis = greedy_decode(model.decoder, model.tgt_embed,
+                                           model.generate, hidden,
+                                           encoder_outputs, encoder_final,
+                                           seq_mask_x, vocab_tgt[SOS_TOKEN], vocab_tgt[EOS_TOKEN],
+                                           vocab_tgt[PAD_TOKEN], hparams.max_decoding_length)
+        else:
+            raw_hypothesis = beam_search(model.decoder, model.tgt_embed, model.generate,
+                                         vocab_tgt.size(), hidden, encoder_outputs,
+                                         encoder_final, seq_mask_x,
+                                         vocab_tgt[SOS_TOKEN], vocab_tgt[EOS_TOKEN],
+                                         vocab_tgt[PAD_TOKEN], hparams.beam_width,
+                                         hparams.length_penalty_factor,
+                                         hparams.max_decoding_length,hparams.n_best)
+
+    hypothesis_l=[]
+    for n in range(raw_hypothesis.size(1)):
+        hypothesis_l.append(batch_to_sentences(raw_hypothesis[:,n,:], vocab_tgt))
+
+    return np.array(hypothesis_l).transpose(1, 0),z
+
+def _evaluate_bleu(model, val_dl, vocab_src, vocab_tgt, device, hparams, compare_with_original=False):
+    model.eval()
+    with torch.no_grad():
+        inputs = []
+        references = []
+        model_hypotheses = []
+        for sentences_x, sentences_y in val_dl:
+            input_sentences_y=None
+            hypothesis_nbest,zs = re_sample(model, sentences_x, vocab_src, vocab_tgt, device, hparams,input_sentences_y=input_sentences_y)
+            hypothesis=[ t_nbest[0] for t_nbest in hypothesis_nbest]
+
+            # Keep track of inputs, references and model hypotheses.
+            inputs += sentences_x.tolist()
+            if compare_with_original:
+                references += sentences_y.tolist()
+            else:
+                references += sentences_x.tolist()
+            model_hypotheses += hypothesis
+
+    bleu = compute_bleu(model_hypotheses, references, subword_token=hparams.subword_token)
+    return bleu, inputs, references, model_hypotheses
+
+def _evaluate_perplexity(model, val_dl, vocab_src, vocab_tgt, device):
+    model.eval()
+    with torch.no_grad():
+        num_predictions = 0
+        num_sentences = 0
+        log_marginal = 0.
+        total_KL = 0.
+        total_KL_prediction = 0.0
+        n_samples = 10
+        for sentences_x, sentences_y in val_dl:
+            x_in, x_out, seq_mask_x, seq_len_x = create_batch(sentences_x, vocab_src, device)
+            y_in, y_out, seq_mask_y, seq_len_y = create_batch(sentences_y, vocab_tgt, device)
+
+            # Infer q(z|x) for this batch.
+            qz = model.approximate_posterior(x_in, seq_mask_x, seq_len_x, y_in, seq_mask_y, seq_len_y)
+            pz = model.prior().expand(qz.mean.size())
+            total_KL += torch.distributions.kl.kl_divergence(qz, pz).sum().item()
+
+            # Take s importance samples from q(z|x):
+            # log int{p(x, y, z) dz} ~= log sum_z{p(x, y, z) / q(z|x)} where z ~ q(z|x)
+            batch_size = x_in.size(0)
+            batch_log_marginals = torch.zeros(n_samples, batch_size)
+            for s in range(n_samples):
+
+                # z ~ q(z|x)
+                z = qz.sample()
+
+                # Compute the logits according to this sample of z.
+                _, lm_logits, _ = model(x_in, seq_mask_x, seq_len_x, y_in, z)
+
+                # Compute log P(x|z_s)
+                log_lm_prob = F.log_softmax(lm_logits, dim=-1)
+                log_lm_prob = torch.gather(log_lm_prob, 2, x_out.unsqueeze(-1)).squeeze()
+                log_lm_prob = (seq_mask_x.type_as(log_lm_prob) * log_lm_prob).sum(dim=1)
+
+                # Compute prior probability log P(z_s) and importance weight q(z_s|x)
+                log_pz = pz.log_prob(z).sum(dim=1) # [B, latent_size] -> [B]
+                log_qz = qz.log_prob(z).sum(dim=1)
+
+                # Estimate the importance weighted estimate of (the log of) P(x, y)
+                batch_log_marginals[s] = log_lm_prob + log_pz - log_qz
+
+            # Average over all samples.
+            batch_log_marginal = torch.logsumexp(batch_log_marginals, dim=0) - \
+                                 torch.log(torch.Tensor([n_samples]))
+            log_marginal += batch_log_marginal.sum().item() # [B] -> []
+            num_sentences += batch_size
+            num_predictions += (seq_len_x.sum() ).item()
+
+    val_NLL = -log_marginal
+    val_perplexity = np.exp(val_NLL / num_predictions)
+    return val_perplexity, val_NLL/num_sentences, total_KL/num_sentences, total_KL_prediction/num_sentences
+
+
+def product_of_gaussians(fwd_base: Normal, bwd_base: Normal) -> Normal:
+    u1, s1, var1 = fwd_base.mean, fwd_base.stddev, fwd_base.variance
+    u2, s2, var2 = bwd_base.mean, bwd_base.stddev, bwd_base.variance
+    u = (var2 * u1 + var1 * u2) / (var1 + var2)
+    s = 1. / (1. / var1 + 1. / var2)
+    return Normal(u, s)
+
+
+def mixture_of_gaussians(fwd_base: Normal, bwd_base: Normal) -> Normal:
+    # [batch_size]
+    selectors = torch.rand(fwd_base.mean.size(0), device=fwd_base.mean.device) >= 0.5
+    # [batch_size, latent_size]
+    selectors = selectors.unsqueeze(-1).repeat([1, fwd_base.mean.size(1)])
+    u = torch.where(selectors, fwd_base.mean, bwd_base.mean)
+    s = torch.where(selectors, fwd_base.stddev, bwd_base.stddev)
+    return Normal(u, s)
