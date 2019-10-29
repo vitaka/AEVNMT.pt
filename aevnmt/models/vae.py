FIx the minus in bow logprobs

aevnmt/models/vae.py
@@ -425,7 +425,9 @@ class VAE(nn.Module):
 
         if bow_logits is not None:
             if self.bernoulli_bow:
-                bow_logprobs,bow_inverse_logprobs = -sparsedists.bernoulli.bernoulli_log_probs_from_logit(bow_logits)
+                bow_logprobs,bow_inverse_logprobs = sparsedists.bernoulli.bernoulli_log_probs_from_logit(bow_logits)
+                bow_logprobs=-bow_logprobs
+                bow_inverse_logprobs=-bow_inverse_logprobs
                 bsz=bow_logits.size(0)
 
                 for i in range(bsz):
@@ -459,7 +461,9 @@ class VAE(nn.Module):
 
         if bow_logits_tl is not None:
             if self.bernoulli_bow:
-                bow_logprobs,bow_inverse_logprobs = -sparsedists.bernoulli.bernoulli_log_probs_from_logit(bow_logits_tl)
+                bow_logprobs_tl,bow_inverse_logprobs_tl = sparsedists.bernoulli.bernoulli_log_probs_from_logit(bow_logits_tl)
+                bow_logprobs_tl=-bow_logprobs_tl
+                bow_inverse_logprobs_tl=-bow_inverse_logprobs_tl
                 bsz=bow_logits_tl.size(0)
                 for i in range(bsz):
                     bow=torch.unique(targets_y[i])