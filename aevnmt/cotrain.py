d89d784699e38001b7d6784d76e4883bea6d2074
Víctor M. Sánchez-Cartagena <vitaka@gmail.com>
2019-11-24 17:18:33 +0100

    Fixed y shuf in cotraining

--- aevnmt/cotrain.py
+++ aevnmt/cotrain.py
@@ -501,7 +501,7 @@ def train(model,
                     x_shuf_in=x_shuf_out=seq_mask_x_shuf=seq_len_x_shuf=noisy_x_shuf_in=None
 
                 if 'shuffled' in model.aux_tms:
-                    y_shuf_in, _shuf_out, seq_mask_y_shuf, seq_len_y_shuf, noisy_y_shuf_in=create_noisy_batch(
+                    y_shuf_in, y_shuf_out, seq_mask_y_shuf, seq_len_y_shuf, noisy_y_shuf_in=create_noisy_batch(
                         sentences_y, vocab_tgt, device,
                         word_dropout=hparams.word_dropout,shuffle_toks=True,full_words_shuf=hparams.shuffle_lm_keep_bpe)
                 else:
